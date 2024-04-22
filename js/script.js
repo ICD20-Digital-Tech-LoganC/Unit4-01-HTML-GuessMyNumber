fb79e0f56f538f59a4582b7941d4fb1ac0e53c0b
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-04-22 03:06:26 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function checkGuess() {
+  let randomNumber = Math.floor((Math.random() * 6) + 1);
+  let userGuess = parseInt(document.getElementById("userGuess").value);
+
+  document.getElementById("guess").textContent = "User Guess: " + userGuess;
+  document.getElementById("answer").textContent = "Computer generated number: " + randomNumber;
+
+  if (userGuess == randomNumber) {
+    document.getElementById("result").textContent = "Congratulations! You guessed the correct number!";
+  } else {
+    document.getElementById("result").textContent = "You guessed the wrong number!";
+  }
+}
